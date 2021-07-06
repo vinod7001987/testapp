d6b3622e8cd5baa46e62d3a1a08f2d9b8fc5e223
vinod7001987 <vinod700@hotmail.com>
2021-07-06 20:57:46 +0530

    Testapp

---
+++ docker/app-src/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<br/><br/><br/>
+    <center>
+    <p><b><?php echo 'Hi, Welcome to DevOps Landing Page'; ?></b></p>
+    <br/>
+    <p><?php echo 'This page is deployed using Azure Devops in Kubernetes Cluster'; ?></p>
+    <br/>
+    <p><b>Release-22</b></p>
+    </center>
+</body>
+</html>
